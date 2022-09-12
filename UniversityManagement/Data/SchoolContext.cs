1dcaaafb375aa354da560ad8322aa1efa039f2ae
hrshuv0 <h.r.shuvo9@gmail.com>
2022-09-12 11:14:03 +0600

    added model class and dbContext

---
+++ UniversityManagement/Data/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityManagement.Models;
+
+namespace UniversityManagement.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- UniversityManagement/Program.cs
+++ UniversityManagement/Program.cs
@@ -1,7 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using UniversityManagement.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<SchoolContext>(options =>
+{
+    options.UseSqlServer(connectionString: connectionString);
+
+    });
+
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+
 
 var app = builder.Build();
 
@@ -11,6 +27,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
 
 app.UseHttpsRedirection();
