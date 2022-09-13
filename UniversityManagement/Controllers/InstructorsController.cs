5bfe28151a4c33cc1597d95218ac31588dd75cdc
hrshuv0 <h.r.shuvo9@gmail.com>
2022-09-13 11:43:34 +0600

    update instructor -  course

--- UniversityManagement/Controllers/InstructorsController.cs
+++ UniversityManagement/Controllers/InstructorsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using Microsoft.EntityFrameworkCore;
 using UniversityManagement.Data;
 using UniversityManagement.Models;
@@ -106,6 +107,8 @@ namespace UniversityManagement.Controllers
 
             var instructor = await _context.Instructors
                 .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments)!
+                .ThenInclude(i => i.Course)
                 .AsNoTracking()
                 .FirstOrDefaultAsync(i => i.ID == id);
             
@@ -114,13 +117,15 @@ namespace UniversityManagement.Controllers
                 return NotFound();
             }
 
+            PopulateAssignedCourseData(instructor);
+
             return View(instructor);
         }
 
         // POST: Instructors/Edit/5
         [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditPost(int? id)
+        public async Task<IActionResult> EditPost(int? id, string[] selectedCourses)
         {
             if (id == null)
             {
@@ -129,6 +134,8 @@ namespace UniversityManagement.Controllers
 
             var instructorToUpdate = await _context.Instructors
                 .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments)!
+                .ThenInclude(i => i.Course)
                 .FirstOrDefaultAsync(s => s.ID == id);
 
             if (await TryUpdateModelAsync<Instructor>(
@@ -140,6 +147,9 @@ namespace UniversityManagement.Controllers
                 {
                     instructorToUpdate.OfficeAssignment = null;
                 }
+
+                UpdateInstructorCourses(selectedCourses, instructorToUpdate);
+
                 try
                 {
                     await _context.SaveChangesAsync();
@@ -153,7 +163,46 @@ namespace UniversityManagement.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
+
+            UpdateInstructorCourses(selectedCourses, instructorToUpdate);
+            PopulateAssignedCourseData(instructorToUpdate);
+
             return View(instructorToUpdate);
+        }
+
+        private void UpdateInstructorCourses(string[] selectedCourses, Instructor instructorToUpdate)
+        {
+            if(selectedCourses == null)
+            {
+                instructorToUpdate.CourseAssignments = new List<CourseAssignment>();
+                return;
+            }
+
+            var selectedCoursesHS = new HashSet<string>(selectedCourses);
+            var instructorCourses = new HashSet<int>(instructorToUpdate.CourseAssignments!.Select(c => c.CourseID));
+
+            foreach(var course in _context.Courses)
+            {
+                if(selectedCoursesHS.Contains(course.CourseID.ToString()))
+                {
+                    if(!instructorCourses.Contains(course.CourseID))
+                    {
+                        instructorToUpdate.CourseAssignments!.Add(new CourseAssignment
+                        {
+                            InstructorID = instructorToUpdate.ID,
+                            CourseID = course.CourseID
+                        });
+                    }
+                }
+                else
+                {
+                    if(instructorCourses.Contains(course.CourseID))
+                    {
+                        CourseAssignment courseToRemove = instructorToUpdate.CourseAssignments!.FirstOrDefault(c => c.CourseID == course.CourseID)!;
+                        _context.Remove(courseToRemove);
+                    }
+                }
+            }
         }
 
         private void PopulateAssignedCourseData(Instructor instructor)
